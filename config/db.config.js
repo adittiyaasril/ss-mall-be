1415c650af8c60e1f801a6df64fb7ee190805c9a
Adittiya Asril <adittiyaasril369@gmail.com>
2024-04-25 06:37:47 +0700

    add .env

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "230798",
-  DB: "mall-db",
-  dialect: "postgres",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
   pool: {
     max: 5,
     min: 0,
